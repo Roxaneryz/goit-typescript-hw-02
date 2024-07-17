9dc8774aa6b44600b5cd110c2df3f9e58b0b4df1
Roxaneryz <roxane5666@yahoo.gr>
2024-07-17 22:18:06 +0300

    +

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,12 +1,19 @@
+@ts-nocheck
+
 import { InfinitySpin } from "react-loader-spinner";
 import css from "./loader.module.css"
 
-
+// interface Props {
+//   visible: boolean;
+//   width: number;
+//   color: string;
+//   ariaLabel: string;
+// }
 
 const Loader = () => {
     return (
         <div className={css.loader}>
-            <InfinitySpin
+            <InfinitySpin 
                 visible={true}
                 width="200"
                 color="#4fa94d"
